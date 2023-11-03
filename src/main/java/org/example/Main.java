541a51171fb23fa05fbb7f4954fd4628bdf696cf
maks245 <mpalamarcuk635@gmail.com>
2023-11-03 21:15:05 +0200

    Add hw 01.11

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,15 @@
 package org.example;
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
-
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com.ua/?hl=uk");
+        String title = driver.getTitle();
+        driver.quit();
+        System.out.println(title);
     }
 }
